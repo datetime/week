9443482df5105f8b2435be1c72992f9b0f538508
jonschlinkert <jon.schlinkert@sellside.com>
2016-12-10 05:40:13 -0500

    run update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * week <https://github.com/jonschlinkert/week>
  *
- * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Copyright (c) 2014-2016 Jon Schlinkert.
  * Licensed under the MIT license.
  */
 
--- test.js
+++ test.js
@@ -1,16 +1,17 @@
 /**
  * week <https://github.com/jonschlinkert/week>
  *
- * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Copyright (c) 2014-2016 Jon Schlinkert.
  * Licensed under the MIT license.
  */
 
-var should = require('should');
+require('mocha');
+var assert = require('assert');
 var moment = require('moment');
 var week = require('./');
 
 describe('week()', function () {
   it('should return an object with the number and name of the current week', function () {
-    week().should.equal(moment(new Date()).format('WW'));
+    assert.equal(week(), moment(new Date()).format('WW'));
   });
 });
